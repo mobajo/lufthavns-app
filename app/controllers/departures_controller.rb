5a69bc979058a4492eeb5df71a86ef44b1e427fd
Morten Jorgensen <morten00@gmail.com>
2018-01-28 15:14:20 +0100

    api updated

--- app/controllers/departures_controller.rb
+++ app/controllers/departures_controller.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class DeparturesController < ApplicationController
   def index
-   source = 'https://infinite-woodland-15476.herokuapp.com/api/v1/arrivals'
+   source = 'https://infinite-woodland-15476.herokuapp.com/api/v1/departures'
    resp = Net::HTTP.get_response(URI.parse(source))
    data = resp.body
    @departures = JSON.parse(data)
